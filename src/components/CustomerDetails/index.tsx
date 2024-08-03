090d47b97ac8ea10f60482d650ed12529d1c6aa0
lichi-dev <bonnybobencr7@gmail.com>
2024-08-03 11:12:57 +0530

    added address in customer details

--- src/components/CustomerDetails/index.tsx
+++ src/components/CustomerDetails/index.tsx
@@ -28,6 +28,7 @@ const CustomerDetails: React.FC<Props> = ({ customer }) => {
   return (
     <div className=" w-[70%] flex flex-col items-center p-5 text-center max-h-[90vh] overflow-auto gap-5">
       <h2 className="text-2xl mt-5">{customer.name}</h2>
+      <h2 className="text-2xl">{customer.address}</h2>
       <p className="text-md w-[70%]">{customer.title}</p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {photos.map((photo, index) => (
